d5a353d80bdc73eb0dd1e653f1b7d6bb43e01c13
Markelas <markgirfanov@gmail.com>
2023-11-23 00:04:02 +0500

    Add components, libraries, fonts

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import 'vuetify/styles'
+import '../src/assets/style.scss'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+createApp(App).use(vuetify).use(VueAxios, axios).mount('#app')
