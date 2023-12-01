3d4790bc5d04ea29e5bf4b8cccfd0e481d8acb08
Markelas <markgirfanov@gmail.com>
2023-12-01 18:10:39 +0500

    Update readme button

--- src/components/StudySession.vue
+++ src/components/StudySession.vue
@@ -30,14 +30,24 @@ export default {
 </div>
   <v-tooltip>
     <template v-slot:activator="{ props }">
-      <v-btn v-bind="props" style="margin-left: 140px">ИНФОРМАЦИЯ</v-btn>
+      <v-btn v-bind="props" class="tooltop__info">ИНФОРМАЦИЯ</v-btn>
     </template>
-    <span class="tooltip_text">Тестовое задание на должность Frontend Developer, где необходимо было получить данные с JSON файла, поместить это все в таблицу и дать возможность отфильтровать с помощью стрелочек на заголовках. В таблицу можно добавлять новые значения.</span>
+    <span class="tooltip_text">Тестовое задание на должность Frontend Developer, где необходимо было получить данные с JSON файла, поместить это все в таблицу и дать возможность отфильтровать с помощью стрелочек на заголовках. В таблицу можно добавлять новые значения. Другие вкладки не функционируют.</span>
   </v-tooltip>
 </template>
 
-<style scoped>
-
+<style>
+.tooltop__info{
+  position: absolute !important;
+  width: 350px;
+  font-size: 30px;
+  min-height: 40px;
+  bottom: 0;
+  right: 0;
+}
+.tooltip_text{
+  font-size: 16px;
+}
 </style>
 
 
